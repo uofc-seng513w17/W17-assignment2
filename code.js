cf048bc979f7adaebc185969b6144acd68a33980
Byron Seto <byronseto@hotmail.com>
2017-02-07 16:18:05 -0700

    Adding a few functions

--- code.js
+++ code.js
@@ -3,9 +3,9 @@
 //
 function getStats(txt) {
     return {
-        nChars: 123,
-        nWords: 22,
-        nLines: 10,
+        nChars: nChars(txt),
+        nWords: nWords(txt),
+        nLines: nLines(txt),
         nNonEmptyLines: 22,
         averageWordLength: 3.3,
         maxLineLength: 33,
@@ -13,5 +13,25 @@ function getStats(txt) {
         longestWords: ["xxxxxxxxx", "123444444"],
         mostFrequentWords: [ "hello(7)", "world(1)" ]
     };
+}
+
+function nChars(txt) {
+    return txt.length;
+}
+
+function nLines(txt) {
+    if (nChars(txt) == 0) {
+        return 0;
+    }
+    return (txt.split("\n").length - 1);
+}
+
+function cleanInput(txt) {
+    return txt.replace(/[|&;$%@"<>()+,#.]/g, "");
+}
+
+function nWords(txt) {
+    clean = cleanInput(txt);
+    return clean
 }
 
